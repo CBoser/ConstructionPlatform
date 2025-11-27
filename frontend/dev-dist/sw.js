07b457dad68046cdc056f55d1e08ff20313ab903
CBoser <coreyboser@gmail.com>
2025-11-27 10:12:12 -0800

    Update

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-237f2c1f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rvsrpauc798"
+    "revision": "0.2aqqgn2m90o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
